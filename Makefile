63abc51b1b5958dc762135026ba052416260f481
ocworld <PAXbRgoTRAZ[AuqXFXdhkcHabtz6hfwnT3#exXm9uekcjmAxfFkhzubXqBaNGBL@>
2018-08-15 20:51:46 +0900

    dev1 버전 구현

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ init:
     pip install -r requirements.txt
 
 test:
+    pip install -r test_requirements.txt
     py.test tests
 
 .PHONY: init test
--- setup.py
+++ setup.py
@@ -12,12 +12,18 @@ with open('LICENSE') as f:
 
 setup(
     name='pyxstr2swift',
-    version='0.1.0',
+    version='0.1.0.dev1',
     description='Python package for a xcode strings file to a swift localized string file',
     long_description=readme,
     author='Keunhyun Oh',
     author_email='ocworld@gmail.com',
     url='https://github.com/ocworld/pyxstr2swift',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
 )
