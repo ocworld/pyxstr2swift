8fa8fa704cfe5e2581230c0047ee688a1c027fa6
ocworld <PAXbRgoTRAZ[AuqXFXdhkcHabtz6hfwnT3#exXm9uekcjmAxfFkhzubXqBaNGBL@>
2018-08-15 21:15:22 +0900

    dev1

---
+++ pyxstr2swift/pyxstr2swift.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import re
+import os
+from os.path import basename, splitext
+
+
+
+def get_keys_from_strings_file(strings_file_path):
+    def comment_remover(text):
+        def replacer(match):
+            s = match.group(0)
+            if s.startswith('/'):
+                return " "  # note: a space and not an empty string
+            else:
+                return s
+
+        pattern = re.compile(
+            r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+            re.DOTALL | re.MULTILINE
+        )
+        return re.sub(pattern, replacer, text)
+
+    with open(strings_file_path, mode='r') as f:
+        contents = f.read()
+
+    lines = comment_remover(contents).splitlines()
+    keys = [line.split('=')[0] for line in lines]
+    filtered_keys = filter(lambda line: line.strip() != "", keys)
+    return list(filtered_keys)
+
+
+def write_keys_to_swift_file(keys, out_file_path, swift_struct_name=""):
+    default_struct_name = splitext(basename(out_file_path))[0]
+    struct_name = swift_struct_name if swift_struct_name != "" else default_struct_name
+    headlines = ["import Foundation", "", "struct %s {" % struct_name]
+    taillines = ["}", ""]
+
+    bodylines = ["  static let %s = \"%s\".localized" % (key, key) for key in keys]
+
+    lines = headlines + bodylines + taillines
+
+    with open(out_file_path, mode='w+') as f:
+        f.write('\n'.join(lines))
+
+
+def xstr2swift(strings_file_path, out_file_path, swift_struct_name="", overwrite_if_out_path_exist=True):
+
+    if os.path.exists(strings_file_path) == False:
+        logging.error('xstr2swift: %s is not exist' % strings_file_path)
+        raise OSError(2)    #ENOENT
+
+    if os.path.exists(out_file_path) == True:
+        if overwrite_if_out_path_exist == True:
+            logging.info('xstr2swift: %s is removed' % out_file_path)
+            os.remove(out_file_path)
+        else:
+            logging.error('xstr2swift: %s is not exist' % out_file_path)
+            raise OSError(17)   #EEXIST
+
+    logging.info('xstr2swift: try to get_keys_from_strings_file(%s)' % out_file_path)
+
+    try:
+        keys = get_keys_from_strings_file(strings_file_path)
+    except IOError as err:
+        logging.error('xstr2swift: failed to get_keys_from_strings_file %s with IOError (no: %d)(err: %s)' % (
+            strings_file_path, err.errno, err.message))
+        raise err
+    except OSError as err:
+        logging.error('xstr2swift: failed to get_keys_from_strings_file %s with OSError (no: %d)(err: %s)' % (
+        strings_file_path, err.errno, err.message))
+        raise err
+    except Exception as ex:
+        logging.error('xstr2swift: failed to get_keys_from_strings_file %s with Exception (no: %d)(err: %s)' % (
+        strings_file_path, ex.errno, ex.message))
+        raise ex
+
+    logging.info('xstr2swift: try to write_keys_to_swift_file(%s)' % out_file_path)
+
+    try:
+        write_keys_to_swift_file(keys, out_file_path, swift_struct_name)
+    except OSError as err:
+        logging.error('xstr2swift: failed to write_keys_to_swift_file %s with os error (no: %d)(err: %s)' % (
+        strings_file_path, err.errno, err.message))
+        raise err
+    except Exception as ex:
+        logging.error('xstr2swift: failed to write_keys_to_swift_file %s with exception (no: %d)(err: %s)' % (
+        strings_file_path, ex.errno, ex.message))
+        raise ex
