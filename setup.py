0166fa7534c1d18fd2953dd327f346086fcaeb25
Keunhyun Oh <ocworld@users.noreply.github.com>
2018-08-15 15:20:34 +0900

    project init

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyxstr2swift',
+    version='0.1.0',
+    description='Python package for a xcode strings file to a swift localized string file',
+    long_description=readme,
+    author='Keunhyun Oh',
+    author_email='ocworld@gmail.com',
+    url='https://github.com/ocworld/pyxstr2swift',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
