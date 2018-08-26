f2a2a94d1f2f45758934779f25c8eec8afde259e
Keunhyun Oh <ocworld@users.noreply.github.com>
2018-08-26 14:34:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyxstr2swift',
-    version='0.1.0.dev16',
+    version='0.1.0.dev17',
     description='Python package for a xcode strings file to a swift localized string file',
     long_description=readme,
     author='Keunhyun Oh',
